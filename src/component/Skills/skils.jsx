b6c0729743182fc27a722072b994e9067d633dac
Aryan <aryankushwahalifenote@gmail.com>
2023-08-01 19:53:03 +0530

    V 1.02.07

--- src/component/Skills/skils.jsx
+++ src/component/Skills/skils.jsx
@@ -4,7 +4,7 @@ const Skils = () => {
     <div className='img w-[100%] h-[100vh] bg-[#131313]'>
       <span className='flex justify-start ml-14 text-fuchsia-950 font-bold border-b-2 w-[200px] border-black text-6xl '>
       Skill-Set
-      </span>
+      </span> 
     </div>
   )
 }
