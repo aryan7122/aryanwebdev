4d95b78141568252fe99b6ad4828d831b8c754a4
Aryan <aryankushwahalifenote@gmail.com>
2023-08-01 13:02:42 +0530

    V 1.02.03

--- src/component/Home/home.jsx
+++ src/component/Home/home.jsx
@@ -3,7 +3,7 @@ import './style.css'
 // import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
 import Typewriter from "typewriter-effect";
 // import img from '../../img/homee/imgg.png'
-import Resume from '../../img/aryanCV.pdf'
+import Resume from '../../img/Aryan_resume.pdf'
 
 const Home = () => {
     let urlgit = 'https://github.com/aryan7122';
@@ -26,17 +26,17 @@ const Home = () => {
                         <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#ccc] opacity-20 "></span>
                         {/* <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span> */}
                     </span>
-                    <span className='text-[#ccc] animate-ping duration-700 text-[1.8rem] p-4'>
+                    <span className='text-[#ccc] animate-ping duration-700 text-[1.8rem] lg:text-[2.2rem]  p-4'>
                         Hi
                     </span>
-                    <span className='text-[#ccc]  text-[1.3rem]'>
+                    <span className='text-[#ccc]  text-[1.3rem] lg:text-[2rem]'>
                         There! I'm
                     </span>
 
                     {/* <h1 className='text-white text-[3.2rem] text font-bold	 '>
                         FRONT END WEB DEVELOPER
                     </h1> */}
-                    <span className='h1 p-4 text-white  text-[1.5rem] sm:text-[2rem] xl:text-[5rem] lg:text-[4rem]  md:text-[3.rem]'>
+                    <span className='h1 p-4 text-white  text-[1.5rem] sm:text-[1.6rem] xl:text-[3rem] lg:text-[3rem]  md:text-[2.5rem]'>
                         <Typewriter 
                             options={{
                                 strings: ['React JS', 'JavaScript', 'Front End Web Developer'],
@@ -48,7 +48,7 @@ const Home = () => {
                     {/* <p className='text-white text-[2.2rem] font-bold	'>
                         
                     </p> */}
-                    <p className='w-full p-4 sm:w-[99%] md:w-[95%] lg:w-[50%] xl:w-[55%] text-[1rem] md:text-[1.3rem] xl:text-[1.7rem] text-slate-500 leading-relaxed  '>
+                    <p className='w-full p-4 sm:w-[99%] md:w-[95%] lg:w-[50%] xl:w-[55%] text-[1rem] md:text-[1rem] xl:text-[1.2rem] text-slate-500 leading-relaxed  '>
                         A Young <span className='text-slate-400'>React JS</span> Developer
                         with crazy from web design. Passionate React JS Developer User Responsive Design. Let's Bring Your Vision to Life!
                     </p>
@@ -59,26 +59,26 @@ const Home = () => {
                         <ul className='flex pl-4  justify-start text-[2rem]'>
                             <li className='pr-8 text-[#ccc] '>
 
-                                <a href={urlgit}><i class="fa-brands fa-square-github animate-bounce "></i></a>
+                                <a href={urlgit}><i class="fa-brands fa-square-github  animate-pulse lg:text-[3rem]"></i></a>
                             </li>
                             <li className='text-blue-800'>
-                                <a href={urllin}><i class="fa-brands fa-linkedin animate-bounce"></i></a>
+                                <a href={urllin}><i class="fa-brands fa-linkedin animate-pulse lg:text-[3rem]"></i></a>
                             </li>
                         </ul>
 
                         <div className='     flex justify-start p-4  '>
-                            <span class="absolute mt-4 flex h-4 w-4  ">
+                            <span class="absolute mt-1  h-4 w-4  ">
                                 <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#ccc] opacity-60 "></span>
-                                <span class="relative inline-flex rounded-full h-4 w-4 bg-[#3000e9]"></span>
+                                <span class="relative animate-bounce inline-flex rounded-full h-4 w-4 -z-10 bg-[#3000e9]"></span>
                             </span>
                             <button onClick={() => menuItemClickHandler("hireme")}
-                                className='border text-[1.2rem]  boder-[#ccc] pl-4 pr-4 pt-2 pb-2 w-42 mt-6 text-[#ccc] hover:text-[#3000e9] hover:border-[#3000e9]  '>
+                                className='border text-[1.2rem]  boder-[#ccc] pl-4 pr-4 pt-2 pb-2 w-42 mt-2 text-[#ccc] hover:text-[#3000e9] hover:border-[#3000e9]  '>
                                 Hire Me!
                             </button>
 
                             <a href={Resume} >
                             <button 
-                                className='ml-10 border text-[1.2rem]  boder-[#ccc] pl-4 pr-4 pt-2 pb-2 w-42 mt-6 text-[#ccc] hover:text-[#3000e9] hover:border-[#3000e9]  '>
+                                className='ml-10 border text-[1.2rem]  boder-[#ccc] pl-4 pr-4 pt-2 pb-2 w-42 mt-2 text-[#ccc] hover:text-[#3000e9] hover:border-[#3000e9]  '>
                                 <i class="fa-solid fa-download pr-2 animate-pulse"></i>
                                 Resume
                             </button>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+const Apps = React.lazy(()=> import ('./App'))
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<span className='flex justify-center items-center w-[100%] h-[100vh]' ><span className='border border-y-sky-900 p-20 rounded-full animate-spin '><div className='w-[60px] h-[60px] rounded-full opacity-40 bg-teal-400 animate-ping'></div></span></span>}>
+    <Apps />
+    </Suspense>
   </React.StrictMode>
 );
